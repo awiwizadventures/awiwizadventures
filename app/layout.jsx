48c808da4c39009a34401b81cf6752a4ada32ecd
dalienst <oduordalienst@gmail.com>
2024-06-10 23:40:37 +0300

    set up bootstrap

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./globals.css";
+import BootstrapClient from "./BootstrapClient";
+
+export const metadata = {
+  title: "Awiwiz Adventures",
+  description: "Taking you places",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="body">
+        {children}
+      <BootstrapClient />
+      </body>
+    </html>
+  );
+}
